3c22e710619ef1980ea2e59aae6f7d8acef116d2
dianewakim <diane.wakim@mediapps.io>
2018-08-23 18:01:59 +0200

    Link the login page to the home page
    We are learning how to use React router and redirects

--- front/user/src/js/login.js
+++ front/user/src/js/login.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import Button from '../../../common/src/js/button';
 
 
 class Login extends Component{
 
+  constructor(props){
+    super(props);
+    this.login = this.login.bind(this);
+  }
+
+  login(){
+    this.props.history.push("/");
+  }
+
   render() {
     return (
       <div>
         <label>email</label><input type="email" />
         <label>password</label><input type="password" />
+        <Link to='/'>Home</Link>
         <Button content="Login!" action={this.login} variant="green" />
       </div>
     )
@@ -18,4 +29,4 @@ class Login extends Component{
 
 }
 
-export default Login;
+export default withRouter(Login);
